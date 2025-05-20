1ce7a809ccb237a4e2d2153d67611c54f832869d
YUNMI LEE <goya3014@hanmail.net>
2025-05-20 13:24:44 +0900

    Make a RestController

---
+++ src/main/java/com/practice/restapi/controller/QuickController.java
@@ -0,0 +1,18 @@
+package com.practice.restapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuickController {
+
+    @GetMapping("/dummy")
+    public String dummy() {
+        return "{}";
+    }
+
+    @GetMapping("/dummy2")
+    public String dummy2() {
+        return "dummy2";
+    }
+}
